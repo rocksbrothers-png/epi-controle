eb7ae1042629fc76a0a2aed01468248c8192f87c
rocksbrothers-png <SEU_EMAIL_DO_GITHUB>
2026-04-01 21:33:00 -0300

    commit 1234567890abcdef1234567890abcdef12345678

--- server_postgres.py
+++ server_postgres.py
@@ -25,7 +25,7 @@ except ModuleNotFoundError:
     DB_CONNECTOR_AVAILABLE = False
     DBIntegrityError = Exception
 
-    def hash_password(password: str) -> str:
+def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password_bytes, salt)
